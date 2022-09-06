23bca607ec3cb68a59cd2aca3b2eaadb3607444e
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-06 12:33:25 +0300

    add Ex13

---
+++ Ex 13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+Console.WriteLine("Ваше число: " + number);
+int num = Convert.ToInt32(number); // Преобразование строки в число для мат операций
+int a = number.Length;
+if (a > 3 || a == 3){
+    while (a > 3)
+    {
+      num = num / 10;
+      a--;  
+    }  
+    int thirdNum = num % 10;
+    Console.WriteLine("Третья цифра: " + thirdNum);
+}
+else if (a < 3){
+    Console.WriteLine("Третьей цифры нет");
+}
