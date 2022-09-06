0352cbe4ee090b358d4baa225411ed345371cd5e
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-06 11:19:33 +0300

    add Ex10

---
+++ Ex 10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ваше число: " + number);
+int firstNum = number / 100;
+int secondNum = (number / 10) % 10;
+int thirdNum = number % 10;
+Console.WriteLine("Вторая цифра: " + secondNum);
+
+
