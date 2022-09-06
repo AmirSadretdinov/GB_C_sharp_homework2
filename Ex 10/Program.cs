3c9e0d22a428ae4156578096c91ea39f35429905
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-06 14:13:58 +0300

    задание 10

--- Ex 10/Program.cs
+++ Ex 10/Program.cs
@@ -3,13 +3,14 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-
+int searchSecondNum(int arg){
+    int result = (arg / 10) % 10;
+    return result;
+}
 Console.WriteLine("Введите трехзначное число");
-int number = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Ваше число: " + number);
-int firstNum = number / 100;
-int secondNum = (number / 10) % 10;
-int thirdNum = number % 10;
-Console.WriteLine("Вторая цифра: " + secondNum);
+int input = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ваше число: " + input);
+int result = searchSecondNum(input);
+System.Console.WriteLine("Вторая цифра: " + result);
 
 
